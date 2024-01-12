28509070c8a2fa16fb77fe4725f64166fa3543ee
yukkop <hectic.yukkop.it@gmail.com>
2024-01-12 11:05:58 +0000

    init

---
+++ default.nix
@@ -0,0 +1,36 @@
+# An example package with dependencies defined via pyproject.toml
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.pip
+  ];
+
+  deps = {nixpkgs, ...}: {
+    python = nixpkgs.python310;
+  };
+
+  inherit (pyproject.project) name version;
+
+  mkDerivation = {
+    src = ./.;
+  };
+
+  buildPythonPackage = {
+    format = lib.mkForce "pyproject";
+    pythonImportsCheck = [
+      "src"
+    ];
+  };
+
+  pip = {
+    pypiSnapshotDate = "2023-08-27";
+    requirementsFiles = [ "./requirements.txt" ]; 
+    flattenDependencies = true;
+  };
+}
